2c8c2481104ec12b812f85adaa74c6c7049efce4
陈首潮 <chenshouchao@chenshouchaos-iMac.local>
2024-03-19 16:21:52 +0800

    401处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -38,6 +38,18 @@ request.interceptors.response.use(
     return response.data;
   },
   (error) => {
+    try {
+      const response = error.response;
+      if (response.status === 401) {
+        // 对路径进行加解密主要不是为了安全问题，而是防止路径中出现多个问号?导致路径参数解析混乱
+        const path = window.location.href;
+        const encryptPath = CryptoJS.AES.encrypt(path, 'path').toString();
+        let url = `${window.location.origin}/login?path=${encryptPath}`;
+        window.location.href = url;
+      }
+    } catch (err) {
+      console.log(err)
+    }
     // 响应错误处理
     return Promise.reject(error);
   },
