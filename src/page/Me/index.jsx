198bd949d7b6ebf5b36387f133a43fd725f04615
invoker <759302142@qq.com>
2024-03-23 20:55:19 +0800

    登录和手机验证码完成

--- src/page/Me/index.jsx
+++ src/page/Me/index.jsx
@@ -2,15 +2,15 @@ import React, { useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 import './index.css';
 
-function Me() {
-  const location = useLocation(); // 获取当前路径信息
-  const showMe = useMemo(() => {
-    return location.pathname !== '/login';
-  }, [location.pathname]);
-  if (showMe) {
-    return <div className="me">个人中心</div>;
-  }
-  return null;
+function Me () {
+    const location = useLocation(); // 获取当前路径信息
+    const showMe = useMemo(() => {
+        return location.pathname !== '/login';
+    }, [location.pathname]);
+    if (showMe) {
+        return <div className="me">个人中心</div>;
+    }
+    return null;
 }
 
 export default Me;
