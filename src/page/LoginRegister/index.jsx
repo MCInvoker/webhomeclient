916997a3761ee35ecc1cbae6c5dba78ae343952e
jiangzijing <jiangzijing@crazymaplestudio.com>
2024-05-24 15:21:53 +0800

    feat: 登录页面布局

--- src/page/LoginRegister/index.jsx
+++ src/page/LoginRegister/index.jsx
@@ -1,63 +1,97 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import classNames from 'classnames';
-import './index.css';
+import { KeyOutlined, UserOutlined } from '@ant-design/icons';
+import { Segmented, Input, Checkbox, Button } from 'antd';
+import { createStyles } from 'antd-style';
 
-import loginhome from '../../image/long.svg';
-
-import AccountPassword from './AccountPassword';
+import loginImg from './login.jpg';
 
 function LoginRegister() {
-  const [loginMethod, setLoginMethod] = useState('AccountPassword'); // MobileCode WeChatQR
+  const { styles } = useStyles();
   return (
-    <div className="login">
-      <div className="loginCenter">
-        <div className="logo">webhome</div>
-        <div className="bottom">
-          <div className="left">
-            <img src={loginhome} alt="" />
+    <div className={styles.loginPage}>
+      <div className={styles.loginBox}>
+        <img src={loginImg} alt="" className={styles.loginImg} />
+        <div className={styles.loginRight}>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+            <div className={styles.loginTitle}>登录</div>
+            <Segmented options={['账号登录', '手机号登录']} className={styles.loginSegmented} />
           </div>
-          <div className="right">
-            <div className="welcome">Welcome Back :)</div>
-            {loginMethod === 'AccountPassword' && <AccountPassword />}
-
-            <div className="loginList">
-              <div
-                className={classNames({
-                  loginLi: true,
-                  loginLiActive: loginMethod === 'AccountPassword',
-                })}
-                onClick={() => setLoginMethod('AccountPassword')}
-              >
-                账号密码
-              </div>
-              <div
-                className={classNames({
-                  loginLi: true,
-                  loginLiActive: loginMethod === 'MobileCode',
-                })}
-                onClick={() => setLoginMethod('MobileCode')}
-              >
-                手机验证码
-              </div>
-              <div
-                className={classNames({
-                  loginLi: true,
-                  loginLiActive: loginMethod === 'WeChatQR',
-                })}
-                onClick={() => setLoginMethod('WeChatQR')}
-              >
-                微信扫码
-              </div>
-            </div>
-            <div className="registerBox">
-              <span className="register">没有账号？立即注册</span>
-            </div>
+          <Input
+            placeholder="请输入账号"
+            prefix={<UserOutlined />}
+            style={{ marginTop: 40, marginBottom: 22 }}
+          />
+          <Input.Password
+            placeholder="请输入密码"
+            prefix={<KeyOutlined />}
+            style={{ marginBottom: 22 }}
+          />
+          <Input placeholder="这里是滚动验证" style={{ marginBottom: 28 }} />
+          <Checkbox style={{ fontSize: 12 }}>记住账号密码</Checkbox>
+          <div style={{ margin: '14px 0 ' }}>
+            <Button type="primary" style={{ width: '100%', height: 32 }}>
+              登录
+            </Button>
+          </div>
+          <div>
+            没有账号？<span style={{ color: '#1E67FA' }}>去注册</span>
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+const useStyles = createStyles(({ css }) => ({
+  loginPage: css`
+    min-width: 100wh;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: linear-gradient(
+      90deg,
+      #edf6ff -1%,
+      #f2fcff 22%,
+      #fcf5ff 41%,
+      #fffbf5 61%,
+      #fff6f6 78%,
+      #eefff7 100%
+    );
+  `,
+  loginBox: css`
+    width: 828px;
+    height: 440px;
+    border-radius: 10px;
+    overflow: hidden;
+    display: flex;
+    background-color: #fff;
+  `,
+  loginImg: css`
+    width: 475px;
+    height: 100%;
+  `,
+  loginRight: css`
+    flex: 1;
+    padding: 61px 56px 0px;
+    .ant-input-outlined {
+      border-color: #ededee;
+    }
+    .ant-input {
+      font-size: 12px !important;
+    }
+  `,
+  loginTitle: css`
+    height: 33px;
+    line-height: 33px;
+    font-size: 24px;
+    font-weight: 500;
+  `,
+  loginSegmented: css`
+    height: 33px;
+    font-size: 12px;
+  `,
+}));
 
 export default LoginRegister;
